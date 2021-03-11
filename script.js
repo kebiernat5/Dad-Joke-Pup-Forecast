54472107ac3ff303a2563b7bfec6a4fed5c0a6e2
kylegavinsimone <kylesimone@outlook.com>
2021-03-11 13:30:17 -0700

    now have the api showing, need inserted into html

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-console.log(fetch("https://icanhazdadjoke.com/")
-  .then(function (response) {
-    // The JSON data will arrive here
-  })
-  .catch(function (err) {
-    // If an error occured, you will catch it here
-  }));
+// fetch("https://icanhazdadjoke.com/")
+//   .then(function (response) {
+//     document.getElementById("dad");
+//   })
+//   .catch(function (err) {
+//     // If an error occured, you will catch it here
+//   });
 
-  var mainContainer = document.getElementById("dad");
-  var jsonStr = JSON.stringify(jsonVar);
+fetch("https://icanhazdadjoke.com/", {
+  headers: {
+    'accept': "application/json",
+  }
+}).then((res) => res.json())
+.then(data => displayData(data));
+
+var mainContainer = document.getElementById("dad");
+
+function displayData(data){
+    console.log(data)
+}
