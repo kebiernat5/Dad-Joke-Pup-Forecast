f2d7cd035e89bf58e7a3c82f7ba69011a5007f5c
kylegavinsimone <kylesimone@outlook.com>
2021-03-11 12:46:03 -0700

    deleted extra script tag that was unfinished, corrected ")" issue

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-fetch("https://icanhazdadjoke.com/")
+console.log(fetch("https://icanhazdadjoke.com/")
   .then(function (response) {
     // The JSON data will arrive here
   })
   .catch(function (err) {
     // If an error occured, you will catch it here
-  });
+  }));
 
   var mainContainer = document.getElementById("dad");
   var jsonStr = JSON.stringify(jsonVar);
