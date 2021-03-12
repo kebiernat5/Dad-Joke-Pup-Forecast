0588ac421bc4d1a7e04fa93a9291aebb6811ba42
Breon Sparrow <beyond.committed@gmail.com>
2021-03-12 11:27:17 -0700

    conflicts

--- script.js
+++ script.js
@@ -66,4 +66,36 @@ function getRandomPup() {
     });
   });
 }
+// function to perform a get request
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+// function to get a random image
+function getRandomImage()
+{   
+  // get the json from the server
+  var json = httpGet('https://random.dog/doggos');
+  console.log(json);
+  // get the image object 
+var image = document.querySelector('.puppy');
+
+  // decode the json into an array
+  var array = JSON.parse(json);
+  console.log(array);
+  
+  // get the image url from the array
+  var url = array.message;
+  for (let index = 0; index < 200; index++) {
+    image.innerHTML = `<img src="https://random.dog/${array[index]}">`;  
+}
+  
+  // set the src of the image object
+  image.src = url;
+}
+getRandomImage();
 
