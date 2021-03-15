2b39b91c33cab1d18dc0bcea36248d4d7eef2bb4
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-03-15 10:48:47 -0700

    forecast date

--- script.js
+++ script.js
@@ -158,7 +158,7 @@ function httpGet(theUrl) {
   
     // Append to forecast Container
     forecastContainer.append(
-      forecastDate,
+      forecastDateEl,
       forecastImg,
       forecastTemp,
       forecastHumidity
